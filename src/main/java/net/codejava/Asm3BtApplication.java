e3bec661dca4a55a3d31967c7473f3e4d7648225
Meliosssss <phamhongquan19122001@gmail.com>
2023-05-16 14:16:48 +0700

    first commit

---
+++ src/main/java/net/codejava/Asm3BtApplication.java
@@ -0,0 +1,14 @@
+package net.codejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Asm3BtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asm3BtApplication.class, args);
+	}
+
+}
